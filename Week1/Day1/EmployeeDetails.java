ba2f2735bf93a18ceaaf16882dfff3130f2c5da0
Karthika-2025 <karthika19.it@gmail.com>
2025-12-07 17:30:31 +0530

    Add files via upload

---
+++ Week1/Day1/EmployeeDetails.java
@@ -0,0 +1,21 @@
+package Week1.Day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args)
+	{
+		String name="Karthika";
+		int age=34;
+		int salary=50000;
+		char gender='F';
+		long phno=9999999999L;
+		
+		// Display the output
+		System.out.println("My name is "+name);
+		System.out.println("Age is "+age);
+		System.out.println("Salary is "+salary);
+		System.out.println("Gender is "+gender);
+		System.out.println("Phone number is "+phno);		
+	}
+
+}
---
+++ Week1/Day2/FindingOddNumbers.java
@@ -0,0 +1,18 @@
+package Week1.Day2;
+
+public class FindingOddNumbers {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Odd numbers from 1 to 10 are ");
+		for (int i = 0; i < 10; i++) 
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
